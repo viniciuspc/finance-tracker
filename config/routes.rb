97d7c9cab15d4d81bfbf8488dedff6625d3ab4e7
Vinicius Carvalho <vinicius.carvalho@fraunhofer.pt>
2020-09-23 16:14:03 +0100

    Add my friends view with listing of fallowed friends

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :user_stocks, only: [:create, :destroy]
+  resources :friendship, only: [:create, :destroy]
   devise_for :users
   root "welcome#index"
   get "my_portfolio", to: "users#my_portfolio"
+  get "my_friends", to: "users#my_friends"
   get "search_stock", to: "stocks#search"
 end
