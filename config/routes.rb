cf628b61218b877f743613b5932f797815b5da48
Vinicius Carvalho <vinicius.carvalho@fraunhofer.pt>
2020-09-23 16:43:57 +0100

    Add search friends forms

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   resources :friendship, only: [:create, :destroy]
   devise_for :users
   root "welcome#index"
-  get "my_portfolio", to: "users#my_portfolio"
-  get "my_friends", to: "users#my_friends"
-  get "search_stock", to: "stocks#search"
+
+  get "my_portfolio",   to: "users#my_portfolio"
+  get "my_friends",     to: "users#my_friends"
+  get "search_friend",  to: "users#search"
+  
+  get "search_stock",   to: "stocks#search"
+  
 end
