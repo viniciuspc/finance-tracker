5973d1c7acd85cd6d693be19cd3243326232edb4
Vinicius Carvalho <vinicius.carvalho@fraunhofer.pt>
2020-09-24 10:02:32 +0100

    Display user/friend profile and tracking options

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  root "welcome#index"
+
   resources :user_stocks, only: [:create, :destroy]
   resources :friendships, only: [:create, :destroy]
-  devise_for :users
-  root "welcome#index"
+  resources :users, only: [:show]
 
   get "my_portfolio",   to: "users#my_portfolio"
   get "my_friends",     to: "users#my_friends"
   get "search_friend",  to: "users#search"
   
   get "search_stock",   to: "stocks#search"
-  
 end
